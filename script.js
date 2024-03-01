91863e87ced05087d0e30e1c72a845a840de7c09
Ibrahim khan <ibrahim2013khan@gmail.com>
2024-03-02 03:10:48 +0600

    This is my first Website only used HTML and CSS

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-	
+setInterval(asynchronous1, 0);
+  function asynchronous1(){
+	let d = new Date();
+
+	  document.getElementById('demoTimer').innerHTML = d.getHours() + ":" + d.getMinutes() + ":" + d.getSeconds();
+ }	
